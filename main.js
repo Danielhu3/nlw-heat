1769006ab5c73857423acfc6b1b390b816362fda
Daniel Barbosa <56881975+Danielhu3@users.noreply.github.com>
2021-10-23 22:33:20 -0300

    primeiros arquivos
    arquivos principais do site

---
+++ main.js
@@ -0,0 +1,37 @@
+const linksSocialMedia = {
+  github: 'danielhu3',
+  youtube: 'DanielMaggot',
+  twitter: 'danielmaggot_',
+  instagram: 'danielmaggot_',
+  facebook: 'maykbrito'
+}
+
+function changeSocialMediaLinks() {
+  for (let li of socialLinks.children) {
+    const social = li.getAttribute('class')
+
+    li.children[0].href = `https://${social}.com/${linksSocialMedia[social]}`
+
+    //alert(li.children[0].href)
+  }
+}
+
+changeSocialMediaLinks()
+
+function getGithubProfileInfos() {
+  const url = `https://api.github.com/users/${linksSocialMedia.github}`
+
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      //let nome = document.getElementById('username')
+
+      username.textContent = data.name
+      userBio.textContent = data.bio
+      userLink.href = data.html_url
+      userImage.src = data.avatar_url
+      userLogin.textContent = data.login
+    })
+}
+
+getGithubProfileInfos()
